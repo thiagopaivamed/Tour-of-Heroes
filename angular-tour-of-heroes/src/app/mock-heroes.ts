752e5349141c42218f74f03820cdd8aed8ba7dad
thiagopaivamed <thiagopaivamed@gmail.com>
2020-07-09 15:26:29 -0300

    first commit

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const Heroes: Hero[] = [
+    {
+        id: 11, name: 'Iron Man'
+    },
+
+    {
+        id: 12, name: 'Viúva Negra'
+    },
+
+    {
+        id: 13, name: 'Hulk'
+    },
+
+    {
+        id: 14, name: 'Doutor Estranho'
+    }
+];
